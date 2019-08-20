33420a95868e247c2510bc9212c5e8460cbbc18f
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:58:33 +0430

    Scroll anim fix

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let top = window.scrollY;
 
         posts.forEach((post, index) => {
-            post.style.top = -((index * (top / posts.length)) + (top / 2)) + 'px';
+            post.style.top = -((index * (top / posts.length)) + (top / 4)) + 'px';
         });
     });
 });
