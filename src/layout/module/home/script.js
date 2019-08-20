e4fae716a85aaf7aa56bbe8cba93785f37c519cf
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:06:54 +0430

    Scroll test

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -3,10 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let posts = document.querySelectorAll('.overview .post');
     
     window.addEventListener('scroll', e => {
-        console.log(e.scrollTop);
-        posts.forEach(post => {
-            console.log(e.scrollTop);
-            post.style.top = -(e.scrollTop / 2) + 'px';
-        });
+        console.log(e);
+        // posts.forEach(post => {
+        //     post.style.top = -(e.scrollTop / 2) + 'px';
+        // });
     });
 });
