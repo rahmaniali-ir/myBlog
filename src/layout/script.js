1031cab13fdada9531a08832a70edffaee5dc805
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 16:55:34 +0430

    test

--- src/layout/script.js
+++ src/layout/script.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let posts = document.querySelectorAll('.overview .post');
 
     window.addEventListener('scroll', e => {
+        console.log(e.scrollTop);
         posts.forEach(post => {
             post.style.top = -(e.scrollTop / 2) + 'px';
         });
