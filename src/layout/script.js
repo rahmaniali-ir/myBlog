f9eedb2d1c25ca5f6a52ec66eadd6bd908482da4
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:03:40 +0430

    Avoid showing images for tset

--- src/layout/script.js
+++ src/layout/script.js
@@ -1,12 +1,12 @@
 
 let images = document.querySelectorAll('img');
 
-images.forEach(image => {
-    image.addEventListener('load', () => {
-        image.classList.add('loaded');
-    });
+// images.forEach(image => {
+//     image.addEventListener('load', () => {
+//         image.classList.add('loaded');
+//     });
 
-    if(image.complete) {
-        image.classList.add('loaded');
-    }
-});
+//     if(image.complete) {
+//         image.classList.add('loaded');
+//     }
+// });
