b41fc525e432e57e5310b3ba7bb6aeec982e7d06
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:05:39 +0430

    Show back images

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -1,13 +1,12 @@
 
-alert('WTF');
-// document.addEventListener('DOMContentLoaded', () => {
-//     let posts = document.querySelectorAll('.overview .post');
+document.addEventListener('DOMContentLoaded', () => {
+    let posts = document.querySelectorAll('.overview .post');
     
-//     window.addEventListener('scroll', e => {
-//         console.log(e.scrollTop);
-//         posts.forEach(post => {
-//             console.log(e.scrollTop);
-//             post.style.top = -(e.scrollTop / 2) + 'px';
-//         });
-//     });
-// });
+    window.addEventListener('scroll', e => {
+        console.log(e.scrollTop);
+        posts.forEach(post => {
+            console.log(e.scrollTop);
+            post.style.top = -(e.scrollTop / 2) + 'px';
+        });
+    });
+});
