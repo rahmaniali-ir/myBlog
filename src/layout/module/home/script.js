d8eccb8391883ccc1fbe4d64cb865b608ceae7b6
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:16:10 +0430

    Overview anim fix

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -1,12 +1,16 @@
 
+const overviewAnimHeight = 200;
+
 document.addEventListener('DOMContentLoaded', () => {
     let posts = document.querySelectorAll('.overview .post');
     
     window.addEventListener('scroll', e => {
         let top = window.scrollY;
 
-        posts.forEach((post, index) => {
-            post.style.top = (-(top / 2) - ((top / 2) * index)) + 'px';
-        });
+        if(top > overviewAnimHeight) {
+            posts.forEach((post, index) => {
+                post.style.top = (-(top / 2) - ((top / 2) * index)) + 'px';
+            });
+        }
     });
 });
