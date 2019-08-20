f4a940f9bca15ccb3aa32ee96be72616fc04b64e
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:00:25 +0430

    Transfer scroll script to home js script

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -1,2 +1,12 @@
 
-
+document.addEventListener('DOMContentLoaded', () => {
+    let posts = document.querySelectorAll('.overview .post');
+    
+    window.addEventListener('scroll', e => {
+        console.log(e.scrollTop);
+        posts.forEach(post => {
+            console.log(e.scrollTop);
+            post.style.top = -(e.scrollTop / 2) + 'px';
+        });
+    });
+});
--- src/layout/script.js
+++ src/layout/script.js
@@ -1,6 +1,6 @@
 
 let images = document.querySelectorAll('img');
-console.log('test');
+
 images.forEach(image => {
     image.addEventListener('load', () => {
         image.classList.add('loaded');
@@ -9,16 +9,4 @@ images.forEach(image => {
     if(image.complete) {
         image.classList.add('loaded');
     }
-});
-
-document.addEventListener('DOMContentLoaded', () => {
-    let posts = document.querySelectorAll('.overview .post');
-    
-    window.addEventListener('scroll', e => {
-        console.log(e.scrollTop);
-        posts.forEach(post => {
-            console.log(e.scrollTop);
-            post.style.top = -(e.scrollTop / 2) + 'px';
-        });
-    });
 });
