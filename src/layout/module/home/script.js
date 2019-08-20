e4654148fbd933c7e95966da86f461289de81cbf
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:11:39 +0430

    Scroll fix

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', () => {
     let posts = document.querySelectorAll('.overview .post');
     
     window.addEventListener('scroll', e => {
-        console.log(window.screenTop);
+        console.log(window.scrollY);
         posts.forEach(post => {
-            post.style.top = -(window.screenTop / 2) + 'px';
+            post.style.top = -(window.scrollY / 2) + 'px';
         });
     });
 });
