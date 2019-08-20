892697ac50f3b4ed9d982b1a285eb5dd4c138102
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:02:01 +0430

    test

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -1,12 +1,13 @@
 
-document.addEventListener('DOMContentLoaded', () => {
-    let posts = document.querySelectorAll('.overview .post');
+alert('WTF');
+// document.addEventListener('DOMContentLoaded', () => {
+//     let posts = document.querySelectorAll('.overview .post');
     
-    window.addEventListener('scroll', e => {
-        console.log(e.scrollTop);
-        posts.forEach(post => {
-            console.log(e.scrollTop);
-            post.style.top = -(e.scrollTop / 2) + 'px';
-        });
-    });
-});
+//     window.addEventListener('scroll', e => {
+//         console.log(e.scrollTop);
+//         posts.forEach(post => {
+//             console.log(e.scrollTop);
+//             post.style.top = -(e.scrollTop / 2) + 'px';
+//         });
+//     });
+// });
