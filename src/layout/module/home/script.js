e96b6feea901fb3c4dbe7e65889c77d369187ed8
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-21 01:19:07 +0430

    Home scroll test

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -1,5 +1,7 @@
 
-const overviewAnimHeight = 300;
+let vh = window.innerHeight / 100;
+
+const overviewAnimHeight = 40 * vh;
 
 document.addEventListener('DOMContentLoaded', () => {
     let posts = document.querySelectorAll('.greeting .post');
@@ -11,5 +13,9 @@ document.addEventListener('DOMContentLoaded', () => {
         posts.forEach((post, index) => {
             post.style.top = -(Math.max((posts.length - index - 1), .5) * (level * (top / 4))) + 'px';
         });
+
+        if(top > 80 * vh) {
+            console.log('Done!');
+        }
     });
 });
