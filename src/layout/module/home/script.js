fc1e02476cf8e884f5ae3bb8e56cf49918507cdd
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-20 17:30:02 +0430

    Scroll anim fix

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         posts.forEach((post, index) => {
             if(top > overviewAnimHeight) {
-                newPos = -(index * level) - top;
+                newPos = top - (index * level);
             }
 
             post.style.top = newPos + 'px';
