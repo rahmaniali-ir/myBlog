4d14f4f4922dd885032427beb878386f1bf5be00
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-22 20:02:45 +0430

    Greeting test

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
         let level = Math.min(1, overviewAnimHeight / top);
 
         posts.forEach((post, index) => {
-            post.style.top = -1 - (top / (50 * vh));
+            post.style.top = -1 - (top / (50 * vh)) + 'px';
         });
 
         goDown.style.opacity = 1 - (top / (50 * vh));
