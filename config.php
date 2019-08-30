acccf7cb0cab37a8a890408cda2676dcc18e4a89
Ali Rahmani <alirahmani.prog@gmail.com>
2019-08-30 14:42:48 +0430

    Protocol fix

--- config.php
+++ config.php
@@ -1,9 +1,8 @@
 <?php
 
 $protocol = $_SERVER['SERVER_PROTOCOL'] == 'HTTP/1.1' ? 'https://' : 'http://';
-echo $protocol;
 // Define constants
-define('SITE_URL', 'http://rahmaniali.ir/');
+define('SITE_URL', $protocol . 'rahmaniali.ir/');
 define('SITE_PATH', __DIR__ . DIRECTORY_SEPARATOR);
 define('SITE_TITLE', 'Ali Rahmani');
 
