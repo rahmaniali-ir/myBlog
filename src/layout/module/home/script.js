b2e95130e2f1e5a4f6f2a5d2cf03c0684c76324d
Ali Rahmani <alirahmani.prog@gmail.com>
2019-09-05 15:46:46 +0430

    Greeting animation removed

--- src/layout/module/home/script.js
+++ src/layout/module/home/script.js
@@ -1,19 +1,10 @@
 
 let vh = window.innerHeight / 100;
 
-const overviewAnimHeight = 40 * vh;
-
 document.addEventListener('DOMContentLoaded', () => {
-    let posts = document.querySelectorAll('.greeting .post');
     let goDown = document.querySelector('.go-down');
     
     window.addEventListener('scroll', () => {
-        let top = window.scrollY;
-
-        posts.forEach((post, index) => {
-            post.style.top = -(top / (2 * vh) * (posts.length - (index + 1))) + 'px';
-        });
-
-        goDown.style.opacity = 1 - (top / (50 * vh));
+        goDown.style.opacity = 1 - (window.scrollY / (50 * vh));
     });
 });
