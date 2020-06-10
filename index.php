d5606ee98ada77490bfdb1ec44d6304ad8d3b706
Ali Rahmani <37767911+rahmaniali-ir@users.noreply.github.com>
2020-06-10 12:50:28 +0430

    get back files

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 // 2020-06-10
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
@@ -7,17 +6,3 @@ ini_set('display_errors', 1);
 require_once('config.php');
 
 loadModule();
-=======
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>علی رحمانی | برنامه نویس فرانت اند</title>
-</head>
-<body>
-    <h1>علی رحمانی</h1>
-</body>
-</html>
->>>>>>> 73df4adbf50ce48f6c70efbe60d6390ef1e48b64
